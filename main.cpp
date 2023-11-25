b3c7372ae960ea420a6953e3a27e5f5ba85465a8
glob <glob@pop-os.localdomain>
2023-11-25 17:02:52 -0300

    cat

--- main.cpp
+++ main.cpp
@@ -17,21 +17,32 @@ using namespace std;
 int block_size=0;
 int inode_size=0;
 
+bool getBit(unsigned short value, int position) // position in range 0-15 for 16 bits
+{
+    return (value >> position) & 0x1;
+}
+
 void printHex(unsigned char byte){
     printf("%02X ", byte);
 }
 
-void printFromFile(fstream& file, int pos, int size){
-  file.seekg(pos);
+void hexDump(fstream* file, int pos, int size){
+  file->seekg(pos);
   // unsigned int dump[size];
   for (int i = 0; i < size; i++) {
-    unsigned char byte;
-    file.read((char*)(&byte), 1 );
-    // printf("%02X ", byte);
+    char byte;
+    file->read((char*)(&byte), 1 );
+    // printf("%c ", byte);
     printHex(byte);
   }
 
   printf("\n");
+}
+void catblock(fstream* file, int pos){
+  file->seekg(pos);
+  char block[block_size];
+  file->read(block, block_size );
+  printf("%s\n", block);
 }
 void print_super_block(ext4_super_block* super_block){
   // printf("Block Count: %d / Blocks p group: %d = %f \n", super_block.s_blocks_count_lo, super_block.s_blocks_per_group, N_G_DESC);
@@ -47,7 +58,7 @@ void print_super_block(ext4_super_block* super_block){
   printf("\n");
 }
 
-void print_block_bitmap(ext4_group_desc* GDT){
+void print_block_desc(ext4_group_desc* GDT){
   printf("bg block bitmap: %d\n", GDT->bg_block_bitmap_lo);
   printf("bg inode bitmap: %d\n", GDT->bg_inode_bitmap_lo);
   printf("bg inode table: %d\n", GDT->bg_inode_table_lo);
@@ -57,14 +68,14 @@ void print_block_bitmap(ext4_group_desc* GDT){
   // FILE_POS= GDT.bg_block_bitmap_lo * block_size;
   // myfile.seekg(FILE_POS);
   // printf("FILE_POS %d :%s\n", 282624, block_bitmap);
-  // printFromFile(myfile, FILE_POS, block_size);
+  // printFromFile(&myfile, FILE_POS, block_size);
 
   //INODE BITMAP
   // FILE_POS= GDT.bg_inode_bitmap_lo * block_size;
   // myfile.seekg(FILE_POS);
   // printf("\nInode BITMAP:%d - %d\n", GDT.bg_inode_bitmap_lo, FILE_POS);
   // printf("FILE_POS %d :%s\n", 282624, block_bitmap);
-  // printFromFile(myfile, FILE_POS, block_size);
+  // printFromFile(&myfile, FILE_POS, block_size);
   
   //INODE TABLE
   //Inode Table Size = No. of Inodes in each Group(sb.s_inodes_per_group) * inode_size
@@ -72,10 +83,21 @@ void print_block_bitmap(ext4_group_desc* GDT){
 }
 
 void print_inode(ext4_inode* inode){
-  printf("block count: %d \n", inode->i_blocks_lo);
+  printf("block count: %d\n", inode->i_blocks_lo);
   printf("i links count: %d \n", inode->i_links_count);
   printf("flags: %X \n", inode->i_flags);
   printf("file mode: %X \n", inode->i_mode);
+
+   unsigned short b = inode->i_mode;
+
+    // Print the bits from position 15 to 0
+    for (int i = 15; i >= 0; --i) {
+        std::cout << getBit(b, i);
+        if (i % 4 == 0) {
+            std::cout << ' '; // Add space every 4 bits for better readability
+        }
+    }
+    printf("\n");
 }
 
 void print_ext_header(ext4_extent_header* ext_header){
@@ -85,12 +107,18 @@ void print_ext_header(ext4_extent_header* ext_header){
   printf("magic: %d \n",ext_header->eh_magic);
 }
 
-void print_ext_header(ext4_extent* ext){
+void print_ext(ext4_extent* ext){
   printf("extends:\n");
   printf("ee_block: %d\n",ext->ee_block);
   printf("ee_len: %d\n",ext->ee_len);
-  printf("ee_start_hi: %d\n",ext->ee_start_hi);
   printf("ee_start_lo: %d\n",ext->ee_start_lo);
+}
+void print_ext_idx(ext4_extent_idx* _idx){
+  printf("extends:\n");
+  printf("ei_block: %d\n",_idx->ei_block);
+  printf("ei_leaf_hi: %d\n",_idx->ei_leaf_hi);
+  printf("ei_leaf_lo: %d\n",_idx->ei_leaf_lo);
+  printf("ei_unused: %d\n",_idx->ei_unused);
 }
 
 void print_dir(fstream* file, int block_position){
@@ -115,15 +143,12 @@ void print_dir(fstream* file, int block_position){
 int find_by_name(fstream* file, int block, char* name){
   int inode_addr= -1;
   int block_position = block * block_size;
-
   // print_dir(file, block_position);
-
-  printf("block:%d, name:%s\n", block_position, name);
+  // printf("block:%d, name:%s\n", block_position, name);
   // return inode_addr;
 
   int block_last = block_position + block_size - 12;
   int FILE_POS = block_position;
-  
 
   // printf("DIR %d - %d\n",FILE_POS, block_last);
   ext4_dir_entry_2 dir;
@@ -173,6 +198,24 @@ void change_dir(fstream* file, int itable_addr, char* inode_name, ext4_inode* cu
   print_dir(file, FILE_POS);
 }
 
+void cat_file(fstream* file, ext4_inode* inode){
+
+  print_inode(inode);
+  ext4_extent_header ext_header;
+  memcpy(&ext_header, inode->i_block, sizeof(ext4_extent_header));
+  ext4_extent cur_ext;
+  memcpy(&cur_ext, &inode->i_block[3], sizeof(ext4_extent));
+
+  ext4_extent_header f_header;
+  memcpy(&f_header, inode->i_block, sizeof(ext4_extent_header));
+  print_ext_header(&f_header);
+  
+  ext4_extent f_ext;
+  memcpy(&f_ext, &inode->i_block[3], sizeof(ext4_extent));
+  print_ext(&f_ext);
+  
+  catblock(file, f_ext.ee_start_lo * block_size);
+}
 
 int main () {
   int FILE_POS=0;
@@ -209,7 +252,7 @@ int main () {
   myfile.seekg(FILE_POS);
   ext4_group_desc GDT;
   myfile.read((char*)(&GDT),  sizeof(GDT) );
-  // print_block_bitmap(&GDT);  
+  // print_block_desc(&GDT);  
 
   // printf("(%d,* %d) + %d\n", GDT.bg_inode_table_lo, block_size, inode_size);
   // printf("INODE 2 POS: %d\n", FILE_POS);
@@ -221,7 +264,7 @@ int main () {
   ext4_inode ROOT;
   myfile.seekg(FILE_POS);
   myfile.read((char*)(&ROOT),  sizeof(ext4_inode) );
-  // print_inode(&inode);
+  // print_inode(&ROOT);
 
   ext4_extent_header ext_header;
   memcpy(&ext_header, ROOT.i_block, sizeof(ext4_extent_header));
@@ -229,21 +272,34 @@ int main () {
   
   ext4_extent ext;
   memcpy(&ext, &ROOT.i_block[3], sizeof(ext4_extent));
-  //is eh_depth == 0 ? ext4_extent : ext4_extent_idxs  
+  //is eh_depth == 0 ? ext4_extent : ext4_extent_idx  
   // ext4_extent ext = *(ext4_extent) &inode.i_block;
-  // print_ext_header(&ext);
+  // print_ext(&ext);
 
   //  block;
   FILE_POS = ext.ee_start_lo * block_size;
-  ext4_dir_entry_2 root;
 
+  // ext4_dir_entry_2 root;
   myfile.seekg(FILE_POS);
-  myfile.read((char*)(&root),  sizeof(ext4_dir_entry_2) );
+  // myfile.read((char*)(&root),  sizeof(ext4_dir_entry_2) );
   // print_dir(&myfile, FILE_POS);
 
-  char* dir_name = "documentos";
-  change_dir(&myfile, itable_initial_addr, dir_name  , &ROOT);
-  
+  char* hello = "hello.txt";
+  int hello_i_addr = find_by_name(&myfile,ext.ee_start_lo,hello);
+  printf("hello_i_addr: %d\n", hello_i_addr);
+  if (hello_i_addr != -1){
+    // printf("hello.txt found, [%d]inode\n",hello_i_addr);
+    FILE_POS = itable_initial_addr + (hello_i_addr * inode_size) - inode_size;
+    
+    myfile.seekg(FILE_POS);
+    ext4_inode f;
+    myfile.read((char*)(&f),  sizeof(ext4_inode) );
+    // print_inode(&f);
+    cat_file(&myfile,&f);
+  }
+
+  // char* dir_name = "documentos";
+  // change_dir(&myfile, itable_initial_addr, dir_name  , &ROOT);  
 
   myfile.close();
   return 0;
